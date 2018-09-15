edb7eb9b077c4935041dd260a279ed8732917152
John Joyce <jjoyce@apple.com>
2018-09-16 01:34:26 +0900

    first

--- setup.py
+++ setup.py
@@ -1,49 +1,45 @@
-from setuptools import find_packages, setup
+import setuptools
+# wraps distutils.core.setup
+# calls also _install_setup_requires(attrs) which calls
+# distutils.core.Distribution(attrs) and runs methods on that.
 
-from pip.req import parse_requirements
-
-
-def get_meta():
-    mod_locals = {}
-
-    execfile(
-        'tornado_console/__about__.py',
-        mod_locals,
-        mod_locals,
-    )
-
-    return dict(
-        (k, v) for k, v in mod_locals.items() if k in mod_locals['__all__']
-    )
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 
-def get_requirements(filename):
-    try:
-        from pip.download import PipSession
-
-        session = PipSession()
-    except ImportError:
-        session = None
-
-    reqs = parse_requirements(filename, session=session)
-
-    return [str(r.req) for r in reqs]
-
-
-meta = get_meta()
-
-
-setup_args = dict(
-    name='tornado-console',
-    version=meta['version'],
-    maintainer=meta['maintainer'],
-    maintainer_email=meta['maintainer_email'],
-    description=meta['description'],
-    url=meta['url'],
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt'),
+setuptools.setup(
+    name="tornado_console2",
+    version="2.0.0",
+    author="dangerwillrobinsondanger",
+    author_email="dangerwillrobinsondanger@gmail.com",
+    description="Console for Tornado apps.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dangerwillrobinsondanger/tornado-console",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='tornado console web python development pip',
+    project_urls={
+        'Documentation':
+            'https://github.com/dangerwillrobinsondanger/tornado-console/wiki',
+        # 'Funding': 'https://donate.pypi.org',
+        # 'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source':
+            'https://github.com/dangerwillrobinsondanger/tornado-console',
+        'Tracker':
+            'https://github.com/dangerwillrobinsondanger/tornado-console/issues',
+    },
+    install_requires=['tornado'],
+    python_requires='>=3.7'
 )
 
 
-if __name__ == '__main__':
-    setup(**setup_args)
+# import distutils.core
+
+# distutils.core.setup(
+#
+# )
--- tornado-console/compat.py
+++ tornado-console/compat.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+'''
 try:
     from io import BytesIO as StringIO
 except ImportError:
